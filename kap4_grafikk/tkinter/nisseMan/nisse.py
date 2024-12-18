1add3e4532615e1f1c74fd474456e9971dec5b13
hmauroy <hmauroy@gmail.com>
2024-12-18 12:38:17 +0100

    Lagde NisseMan

---
+++ kap4_grafikk/tkinter/nisseMan/nisse.py
@@ -0,0 +1,19 @@
+"""Klasse for NisseMan (pacman med nissedrakt)"""
+from tkinter import PhotoImage
+class Nisse:
+    def __init__(self):
+        self.bilde = PhotoImage(file="nissen_small.png")
+        self.x = 100
+        self.y = 45
+        self.x_retning = 0
+        self.y_retning = 1
+        self.fart = 5
+    
+    def oppdater(self):
+        self.x += self.x_retning * self.fart
+    
+    def tegn(self, canvas):
+        canvas.coords(self.bilde, self.x, self.y)
+        
+
+
